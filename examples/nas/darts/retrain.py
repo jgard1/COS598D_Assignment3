1fa8a4c6a67722b0408488be8a8c02fcca031ced
Josh <44043414+jgard1@users.noreply.github.com>
2021-04-28 12:40:41 -0400

    yeetos bettos

--- examples/nas/darts/retrain.py
+++ examples/nas/darts/retrain.py
@@ -117,7 +117,7 @@ if __name__ == "__main__":
     parser.add_argument("--layers", default=20, type=int)
     parser.add_argument("--batch-size", default=96, type=int)
     parser.add_argument("--log-frequency", default=10, type=int)
-    parser.add_argument("--epochs", default=600, type=int)
+    parser.add_argument("--epochs", default=100, type=int)
     parser.add_argument("--aux-weight", default=0.4, type=float)
     parser.add_argument("--drop-path-prob", default=0.2, type=float)
     parser.add_argument("--workers", default=4)
---
+++ examples/nas/darts/run_darts.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+python search.py --v1 --visualization > darts_search_output.txt
+
+CHECKPOINT_PREFIX="./checkpoints/epoch_"
+CHECKPOINT_SUFFIX=".json"
+
+OUTPUT_PREFIX="darts_final_retrain_"
+OUTPUT_SUFFIX=".txt"
+
+for EPOCH in "0" "20" "40" "60" "80" "99" "10" "30" "50" "70" "90"    ### Outer for loop ###
+do
+    CHECKPOINT="${CHECKPOINT_PREFIX}${EPOCH}${CHECKPOINT_SUFFIX}"
+    OUTFILE="${OUTPUT_PREFIX}${EPOCH}${OUTPUT_SUFFIX}"
+    python retrain.py --arc-checkpoint $CHECKPOINT > $OUTFILE
+done
+
